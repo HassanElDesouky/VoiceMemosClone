4f1ae1b3934810da590770521b4c53c96cb04918
hassaneldesouky <hassaneldesouky@icloud.com>
2019-01-12 01:56:43 +0200

    Initial Commit

---
+++ VoiceMemosClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceMemosClone
+//
+//  Created by Hassan El Desouky on 1/12/19.
+//  Copyright © 2019 Hassan El Desouky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
